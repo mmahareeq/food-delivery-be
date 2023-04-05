aa7e57f8733b28ceaf7059dc7970b7b4b2927687
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-04-06 01:32:48 +0300

    create a category

--- model/category.js
+++ model/category.js
@@ -5,6 +5,11 @@ const categorySchema = new Schema({
    name: {
     type: String
    },
+   status: {
+      type: String,
+      enum: ['enabled', 'disabled'],
+      default: 'enabled'
+   }
 }, { timestamps: true });
 
 module.exports = mongoose.model('CategoryModel', categorySchema);
