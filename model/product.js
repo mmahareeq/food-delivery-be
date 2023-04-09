075b45b047e8fe48403e9ef9af778a5d1576c8cc
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-04-10 01:19:49 +0300

    delete img file after deleting ite,

--- model/product.js
+++ model/product.js
@@ -23,8 +23,29 @@ const productSchema = new Schema({
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'CategoryModel',
+    },
+    reviews: {
+        type: [{
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'UserModel'},
+            rating:  {type: Number, min: 1, max: 5 },
+            body: {type: String},
+
+        }]
+    },
+    Rating : { 
+        type: Number
     }
-    
+
 });
+productSchema.virtual('averageRating').get(function () {
+    if (this.reviews.length === 0) {
+      return 0;
+    }
+    const totalRating = this.reviews.reduce((acc, review) => acc + review.rating, 0);
+    return totalRating / this.reviews.length;
+  });
+  
 
 module.exports = mongoose.model('ProductModel', productSchema);
