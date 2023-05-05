5a30a77c771e7af5d4227b8aa1ded91fd8daa15e
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-05-06 00:15:00 +0300

    update white list

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://food-delivery-1.onrender.com'
+    'https://food-delivery-1.onrender.com',
+    'https://prod-branch--bespoke-twilight-e5528a.netlify.app/'
 ]
 
 module.exports = allowedOrigins
